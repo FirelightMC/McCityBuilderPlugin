2b2ca24ca734f5b9dd2e790280322941ab73aef8
Foxxite <foxxite@foxxite.com>
2021-09-14 22:56:18 +0200

    Boilerplate

---
+++ src/main/java/com/foxxite/emptyplugin/EmptyPlugin.java
@@ -0,0 +1,124 @@
+package com.foxxite.emptyplugin;
+
+import com.foxxite.emptyplugin.events.EventListener;
+import com.foxxite.emptyplugin.misc.CommandHandler;
+import com.foxxite.emptyplugin.tasks.Task;
+import com.foxxite.fxcore.config.Config;
+import com.foxxite.fxcore.config.Language;
+import com.foxxite.fxcore.misc.UpdateChecker;
+import com.foxxite.fxcore.sql.SQLHandler;
+import lombok.Getter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginLogger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Timer;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+import static com.foxxite.fxcore.misc.UpdateChecker.UpdateCheckResult.UP_TO_DATE;
+
+public class EmptyPlugin extends JavaPlugin {
+
+    // Premium Placeholders
+    public static final String RESOURCE_ID = "%%__RESOURCE__%%";
+    public static final String USER_ID = "%%__USER__%%";
+    public static final String NONCE = "%%__NONCE__%%";
+
+    // Update checker code
+    @Getter
+    private final int spigotResourceID = 0;
+    @Getter
+    private UpdateChecker updateChecker;
+
+    // Other Stuff
+    private Timer timer = new Timer();
+    @Getter
+    private SQLHandler sqlHandler;
+    @Getter
+    private Language language;
+    @Getter
+    private Config configRaw;
+    @Getter
+    private FileConfiguration configuration;
+    @Getter
+    private PluginLogger pluginLogger;
+
+    // Behaviour Code
+    private EventListener eventListener;
+    private CommandHandler commandHandler;
+
+    @Override
+    public void onEnable() {
+
+        this.pluginLogger = new PluginLogger(this);
+
+        //Register config files
+        this.language = new Language(this);
+        this.configRaw = new Config(this);
+        this.configuration = this.configRaw.getConfig();
+
+        //Update Checker
+        this.updateChecker = new UpdateChecker(this.spigotResourceID, this);
+        this.showUpdateMessage();
+
+        //Register SQL handler
+        this.sqlHandler = new SQLHandler(this, new ArrayList<>());
+
+        //Register commands
+        this.commandHandler = new CommandHandler(this);
+        this.getCommand("command").setExecutor(this.commandHandler);
+
+        //Register Timers
+        this.timer.schedule(new Task(this), 0, 1000);
+
+        //Register event listeners
+        this.eventListener = new EventListener();
+        this.getServer().getPluginManager().registerEvents(this.eventListener, this);
+
+        this.pluginLogger.log(new LogRecord(Level.INFO, "Foxxite's plugin enabled"));
+
+    }
+
+    @Override
+    public void onDisable() {
+        this.timer.cancel();
+        this.timer = null;
+
+        this.getCommand("command").setExecutor(null);
+        this.commandHandler = null;
+
+        this.sqlHandler.closeConnection();
+        this.sqlHandler = null;
+
+        this.pluginLogger.log(new LogRecord(Level.INFO, "Foxxite's plugin disabled"));
+
+    }
+
+    public void reloadConfig() {
+        this.configuration = this.configRaw.getConfig();
+    }
+
+    private void showUpdateMessage() {
+        if (this.updateChecker.getUpdateCheckResult() != (UP_TO_DATE)) {
+            final HashMap<String, String> placeholders = new HashMap<>();
+
+            final String newVersion = (this.updateChecker.getLatestVersionString() != null ? this.updateChecker.getLatestVersionString() : "N/A");
+            final String updateUrl = (this.updateChecker.getResourceURL() != null ? this.updateChecker.getResourceURL() : "N/A");
+
+            placeholders.put("{newVersion}", newVersion);
+            placeholders.put("{updateUrl}", updateUrl);
+            placeholders.put("{checkResult}", this.updateChecker.getUpdateCheckResult().toString());
+
+            final List<String> updateMSG = this.language.getMultiLineMessageCustom("update", placeholders);
+            for (final String message : updateMSG) {
+                this.pluginLogger.info(message);
+            }
+        } else {
+            this.pluginLogger.info("Plugin is up to date.");
+        }
+    }
+}
---
+++ src/main/java/com/foxxite/emptyplugin/misc/CommandHandler.java
@@ -0,0 +1,103 @@
+package com.foxxite.emptyplugin.misc;
+
+import com.foxxite.emptyplugin.EmptyPlugin;
+import com.foxxite.fxcore.config.Config;
+import com.foxxite.fxcore.config.Language;
+import com.foxxite.fxcore.misc.Common;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandHandler implements TabExecutor {
+
+    private final EmptyPlugin plugin;
+    private final Config config;
+    private final Language language;
+
+    public CommandHandler(final EmptyPlugin plugin) {
+        this.plugin = plugin;
+        this.config = plugin.getConfigRaw();
+        this.language = plugin.getLanguage();
+    }
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+        if (command.getName().equalsIgnoreCase("command")) {
+            if (sender instanceof Player) {
+                final Player player = (Player) sender;
+                if (args.length > 0) {
+                    switch (args[0]) {
+                        case "reload":
+                            if (player.hasPermission("plugin.reload")) {
+                                this.config.reloadConfig();
+                                this.plugin.reloadConfig();
+                                this.language.reloadLanguage();
+                                player.sendMessage(this.language.getMessage("reload"));
+                            } else {
+                                player.sendMessage(this.language.getMessage("no-perms"));
+                            }
+                            break;
+                        default:
+                            player.sendMessage(this.language.getMessage("unknown-command"));
+                            break;
+                    }
+                    return true;
+                }
+            } else {
+                sender.sendMessage(Common.colorize("&cYou have to be a player to use this command."));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private ArrayList<String> getSubCommands() {
+        final ArrayList<String> returns = new ArrayList<>();
+        returns.add("command1");
+        returns.add("command2");
+        returns.add("command3");
+        returns.add("reload");
+
+        returns.sort(String::compareToIgnoreCase);
+        return returns;
+    }
+
+    @Override
+    public List<String> onTabComplete(final CommandSender sender, final Command command, final String label, final String[] agrs) {
+
+        if (agrs.length > 0) {
+            ArrayList<String> autoComplete = new ArrayList<>();
+            int activeArg = 0;
+
+            if (agrs.length == 1) {
+
+                activeArg = 0;
+                if (agrs[0].length() == 0) {
+                    return this.getSubCommands();
+                }
+
+                autoComplete = this.getSubCommands();
+
+            }
+
+            final ArrayList<String> returnList = new ArrayList<>();
+
+            //Intelligent Auto Complete
+            for (final String subCommand : autoComplete) {
+                //Check if args contain subcommand, ignore case
+                if (subCommand.startsWith(agrs[activeArg]) || subCommand.toLowerCase().startsWith(agrs[activeArg])) {
+                    returnList.add(subCommand);
+                }
+            }
+
+            return returnList;
+        }
+
+        return null;
+    }
+
+}

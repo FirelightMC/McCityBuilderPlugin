8158395459e51dd416fd676503c8e74769616ef6
Bram <bram.gussekloo@gmail.com>
2021-09-14 23:08:23 +0200

    Update to java 16

--- src/main/java/com/foxxite/emptyplugin/events/EventListener.java
+++ src/main/java/com/foxxite/emptyplugin/events/EventListener.java
@@ -3,11 +3,12 @@ package com.foxxite.emptyplugin.events;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class EventListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEvent(final Event event) {
+    public void onEvent(final PlayerJoinEvent event) {
 
     }
 
